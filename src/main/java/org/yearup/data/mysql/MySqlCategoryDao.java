3fb45433298c9f8f3678b142d975f02d2a493310
Yohannes Kassaye <jmule1567@gmail.com>
2025-06-25 13:36:33 -0700

    Fix get categories

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,11 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -20,8 +23,21 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public List<Category> getAllCategories()
     {
-        // get all categories
-        return null;
+        List<Category> categoryList = new ArrayList<>();
+        String query = "SELECT * FROM categories";
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement ps = connection.prepareStatement(query);
+                ResultSet resultSet = ps.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                categoryList.add(mapRow(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categoryList;
     }
 
     @Override
